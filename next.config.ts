7e27862feed28d3cccf8e9c4f190ce0285262418
gunsow <fukudatatsuya911@gmail.com>
2026-01-16 21:24:14 +0900

    githubpagesへのデプロイworkflowを追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/local-opendata-abu" : "",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/local-opendata-abu/" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
